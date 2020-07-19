7a79211019e93fecf518e2515423037ce8e4ad72
liyf <114188354@qq.com>
2020-07-19 12:23:12 +0900

    创建LoginViewController

--- git/LIVE812x/LIVE812x/Login/Controller/LoginViewController.swift
+++ git/LIVE812x/LIVE812x/Login/Controller/LoginViewController.swift
@@ -13,6 +13,7 @@ class LoginViewController: BaseController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
     

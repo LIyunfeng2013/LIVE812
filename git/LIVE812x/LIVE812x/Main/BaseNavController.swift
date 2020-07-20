6f0229d0c082b6a5dbc6027518115a2c732bd0b0
liyunfeng <114188354@qq.com>
2020-07-20 12:01:45 +0900

    在公司修改

--- git/LIVE812x/LIVE812x/Main/BaseNavController.swift
+++ git/LIVE812x/LIVE812x/Main/BaseNavController.swift
@@ -16,14 +16,9 @@ class BaseNavController: UINavigationController {
         super.viewDidLoad()
 
         self.navigationBar.barTintColor = .white
-        #if swift(>=4.0)
-        self.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.init(red: 38/255.0, green: 38/255.0, blue: 38/255.0, alpha: 1.0), NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16.0)]
-        #elseif swift(>=3.0)
-        self.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.init(red: 38/255.0, green: 38/255.0, blue: 38/255.0, alpha: 1.0), NSFontAttributeName: UIFont.systemFont(ofSize: 16.0)];
-        #endif
+
         self.navigationBar.tintColor = UIColor.init(red: 38/255.0, green: 38/255.0, blue: 38/255.0, alpha: 1.0)
-        
-        // Do any additional setup after loading the view.
+
         self.interactivePopGestureRecognizer?.delegate = self
     }
     
